a0c9758d5e1b07243879da1c6b76ebbe099203ac
Chidi Nweke <chidi125@gmail.com>
2023-10-23 02:31:41 +0200

    Complete part 2 of day 6

--- src/main/scala/day6/tuningTrouble.scala
+++ src/main/scala/day6/tuningTrouble.scala
@@ -16,7 +16,16 @@ import utils.readFile
     .getOrElse(throw Exception("Did not find anything."))
     .force
 
+  val sequence2 = input.view
+    .sliding(size = 14)
+    .find(group => group.toList.distinct.size == 14)
+    .getOrElse(throw Exception("Did not find anything."))
+    .force
+
   val position = input.view.sliding(4).indexWhere(a => a.toVector == sequence)
+  val position2 =
+    input.view.sliding(14).indexWhere(a => a.toVector == sequence2)
 
   println(sequence)
   println(position + 4) // we start at 1790, we need to go 4 more forward
+  println(position2 + 14)
